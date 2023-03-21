3b5e919028cb443ff1b5a244a9bedfa184f56f46
Sarthak Roy <sarthakroy2002@gmail.com>
2023-03-21 18:13:39 +0530

    Drop features which aren't working
    * Will revisit later

--- app/src/main/java/com/neolit/simpledeviceinfo/MainActivity.java
+++ app/src/main/java/com/neolit/simpledeviceinfo/MainActivity.java
@@ -60,23 +60,6 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add("Build Fingerprint: "+ Build.FINGERPRINT);
         arrayList.add("Build ID: "+ Build.ID);
         arrayList.add("Build Type: "+ Build.TYPE);
-        if (!Objects.equals(getProperty("ro.treble.enabled"), "true")) {
-            arrayList.add("Project Treble: true");
-        }else{
-            arrayList.add("Project Treble: false");
-        }
-        if (Objects.equals(getProperty("ro.virtual_ab.enabled"), "true") && Objects.equals(getProperty("ro.virtual_ab.retrofit"), "false")) {
-            arrayList.add("Virtual A/B Partition Scheme: true");
-        }
-        else if (Objects.equals(getProperty("ro.build.ab_update"), "true")) {
-            arrayList.add("A/B Partition Scheme: true");
-        }
-        else if (Objects.equals(getProperty("ro.virtual_ab.retrofit"), "true")) {
-            arrayList.add("Virtual A/B Retrofit Partition Scheme: true");
-        }
-        else{
-            arrayList.add("A-only Partition Scheme: true");
-        }
         arrayList.add("RAM: "+ totalMemory + "MB");
 
         adapter.notifyDataSetChanged();
